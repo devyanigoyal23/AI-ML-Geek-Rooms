3f62eac833fafb648a9e4bfb3e7e058b6f327f35
devyanigoyal23 <devyanigoyal23@gmail.com>
2026-09-23 22:10:51 +0530

    Add files via upload

---
+++ PROBLEM2.py
@@ -0,0 +1,6 @@
+#Install an external module and use it to perform an operstion of your interest.
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello, I am your AI assistant. How can I help you today?")
+engine.runAndWait()
